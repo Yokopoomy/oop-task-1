1636503266a914ac88c14d3d5a08cef994f62e9d
Evgeny Golovin <yokopoomy20@gmail.com>
2023-10-18 14:43:15 +0300

    Task 1

---
+++ src/js/__tests__/character.test.js
@@ -0,0 +1,95 @@
+import * as characters from '../character';
+
+test('create Bowerman', () => {
+  const bowerman = new characters.Bowerman('Char1');
+	
+  expect(bowerman).toEqual(
+		{
+			"name": "Char1",
+			"health": 100,
+			"level": 1,
+			"type": "Bowerman",
+			"attack": 25,
+			"defence": 25
+		}
+	);
+});
+
+test('create Swordsman', () => {
+	const swordsman = new characters.Swordsman('Char2');
+	
+  expect(swordsman).toEqual(
+		{
+			"name": "Char2",
+			"health": 100,
+			"level": 1,
+			"type": "Swordsman",
+			"attack": 40,
+			"defence": 10
+		}
+	);
+});
+
+test('create Magician', () => {
+	const magician = new characters.Magician('Char3');
+	
+  expect(magician).toEqual(
+		{
+			"name": "Char3",
+			"health": 100,
+			"level": 1,
+			"type": "Magician",
+			"attack": 10,
+			"defence": 40
+		}
+	);
+});
+
+test('create Undead', () => {
+	const undead = new characters.Undead('Char4');
+	
+  expect(undead).toEqual(
+		{
+			"name": "Char4",
+			"health": 100,
+			"level": 1,
+			"type": "Undead",
+			"attack": 25,
+			"defence": 25
+		}
+	);
+});
+
+test('create Zombie', () => {
+	const zombie = new characters.Zombie('Char5');
+	
+  expect(zombie).toEqual(
+		{
+			"name": "Char5",
+			"health": 100,
+			"level": 1,
+			"type": "Zombie",
+			"attack": 40,
+			"defence": 10
+		}
+	);
+});
+
+test('create Daemon', () => {
+	const daemon  = new characters.Daemon ('Char6');
+	
+  expect(daemon).toEqual(
+		{
+			"name": "Char6",
+			"health": 100,
+			"level": 1,
+			"type": "Daemon",
+			"attack": 10,
+			"defence": 40
+		}
+	);
+});
+
+test('check name length', () => {
+  expect(new characters.Bowerman('CharacterName')).toThrow();
+});
---
+++ src/js/character.js
@@ -0,0 +1,68 @@
+class Character {
+	constructor(name) {
+		this.setName(name);
+		this.health = 100;
+		this.level = 1;
+	}
+
+	setName(name) {
+		if (name.length > 10) {
+			throw new Error('Name must be between 2 and 10 characters');
+		}
+		this.name = name;
+	}
+}
+
+export class Bowerman extends Character {
+	constructor(name, health, level) {
+		super(name, health, level);
+		this.type = 'Bowerman';
+		this.attack = 25;
+		this.defence = 25;
+	}
+}
+
+export class Swordsman extends Character {
+	constructor(name, health, level) {
+		super(name, health, level);
+		this.type = 'Swordsman';
+		this.attack = 40;
+		this.defence = 10;
+	}
+}
+
+export class Magician extends Character {
+	constructor(name, health, level) {
+		super(name, health, level);
+		this.type = 'Magician';
+		this.attack = 10;
+		this.defence = 40;
+	}
+}
+
+export class Undead extends Character {
+	constructor(name, health, level) {
+		super(name, health, level);
+		this.type = 'Undead';
+		this.attack = 25;
+		this.defence = 25;
+	}
+}
+
+export class Zombie extends Character {
+	constructor(name, health, level) {
+		super(name, health, level);
+		this.type = 'Zombie';
+		this.attack = 40;
+		this.defence = 10;
+	}
+}
+
+export class Daemon extends Character {
+	constructor(name, health, level) {
+		super(name, health, level);
+		this.type = 'Daemon';
+		this.attack = 10;
+		this.defence = 40;
+	}
+}
